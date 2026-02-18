9119c3947031d1ad04e13b6d3eb60d7e18a62567
sadegh-it <sadeghassassin@gmail.com>
2026-02-18 16:26:42 +0330

    chore: add core project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.plugin)
 }
 
 android {
@@ -52,6 +54,25 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.coroutine.android)
+    implementation(libs.coroutine.core)
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation(libs.gson.converter)
+    implementation(libs.lifecycle)
+    implementation(libs.hilt.android)
+    implementation(libs.datastore)
+    implementation(libs.navigation)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.androidx.work)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.okhttp)
+
+    ksp(libs.room.compiler)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
